af04ef644d60353e03ff7ce336cfb2a6367a8940
Dany <ddanygagnon@gmail.com>
2017-11-07 18:55:25 -0500

    Added title-bar component
    The title-bar component with styles.

---
+++ title-bar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="title-bar">
+        <span class="title-bar__name">Dany</span>
+        <span class="title-bar__options">
+            <span class="title-bar__item" @click="minimize">-</span>
+            <span class="title-bar__item" @click="maximize">{}</span>
+            <span class="title-bar__item title-bar__item--exit" @click="exit">x</span>
+        </span>
+    </div>
+</template>
+
+<script>
+    const { remote } = require('electron')
+    export default {
+        name: 'title-bar',
+        methods: {
+            exit() { remote.getCurrentWindow().close() },
+            maximize() {
+                if(remote.getCurrentWindow().isMaximized())
+                    remote.getCurrentWindow().unmaximize()
+                else
+                    remote.getCurrentWindow().maximize()
+            },
+            minimize() { remote.getCurrentWindow().minimize() }
+        }
+    }
+</script>
